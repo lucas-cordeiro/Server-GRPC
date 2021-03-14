74f86821d91cc3c6f02f478e3d26c1e3c3bcae4d
Lucas Cordeiro <lucas.cordeiro5@etec.sp.gov.br>
2021-03-14 00:32:30 -0300

    feat: GetBankAccountCoins

--- src/main/kotlin/KleverServer.kt
+++ src/main/kotlin/KleverServer.kt
@@ -3,22 +3,33 @@ package br.com.lucascordeiro.klever
 import br.com.lucascordeiro.klever.*
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.ListenerRegistration
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.sendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import java.io.FileInputStream
+import kotlin.coroutines.CoroutineContext
 
 
-class KleverServer(val port: Int, val firestore: Firestore) {
+class KleverServer(val port: Int, val firestore: Firestore): CoroutineScope {
+
+    override val coroutineContext = Dispatchers.IO
+
     val server: Server = ServerBuilder
         .forPort(port)
-        .addService(KleverService(firestore))
+        .addService(KleverService(firestore, this))
         .build()
 
     fun start() {
@@ -34,6 +45,7 @@ class KleverServer(val port: Int, val firestore: Firestore) {
     }
 
     private fun stop() {
+        coroutineContext.cancel()
         server.shutdown()
     }
 
@@ -41,10 +53,10 @@ class KleverServer(val port: Int, val firestore: Firestore) {
         server.awaitTermination()
     }
 
-    private class KleverService(val firestore: Firestore) : KleverServiceGrpcKt.KleverServiceCoroutineImplBase() {
+    private class KleverService(val firestore: Firestore, scope: CoroutineScope) : KleverServiceGrpcKt.KleverServiceCoroutineImplBase() {
         override fun getBankAccount(request: GetBankAccountRequest): Flow<BankAccount> = callbackFlow {
             val bankAccountId = request.bankAccountId.toString()
-            println("GetBankAccount: $bankAccountId")
+            println("getBankAccount: $bankAccountId")
 
             val bankAccountRef = firestore.collection("bankaccounts").document(bankAccountId)
 
@@ -53,7 +65,6 @@ class KleverServer(val port: Int, val firestore: Firestore) {
                     throw error
 
                 if (snapshot != null && snapshot.exists()) {
-                    println("exist")
                     val bankAccount = BankAccount.newBuilder()
                         .setId(snapshot.id.toLong())
                         .setBalance(snapshot.getDouble("balance")?:0.0)
@@ -69,6 +80,111 @@ class KleverServer(val port: Int, val firestore: Firestore) {
             awaitClose {
                 println("close")
                 listenerRegistration.remove()
+            }
+        }
+
+        override fun getBankAccountCoins(request: GetBankAccountCoinsRequest): Flow<ListOfBankAccountCoin> = callbackFlow {
+            val bankAccountId = request.bankAccountId.toString()
+            println("getBankAccountCoins: $bankAccountId")
+
+            val bankAccountCoinsResponse: MutableList<BankAccountCoin> = ArrayList()
+
+            val coinsRef =  firestore.collection("coins").whereArrayContains("bankAccountsId", request.bankAccountId)
+            val coinsListener = coinsRef.addSnapshotListener { coinsSnapshot, error ->
+                if (error != null)
+                    throw error
+
+                if (coinsSnapshot != null && coinsSnapshot.documents.isNotEmpty()) {
+                    val coins = coinsSnapshot.documents.map {
+                        Coin.newBuilder()
+                            .setId(it.id.toLong())
+                            .setName(it.getString("name"))
+                            .setShortName(it.getString("shortName"))
+                            .setPrice(it.getDouble("price")?:0.0)
+                            .setPercent(it.getDouble("percent")?.toFloat()?:0f)
+                            .setIconUrl(it.getString("iconUrl"))
+                            .build()
+                    }
+
+                    println("Coins: ${coins.size}")
+
+                    coins.forEach { coin ->
+                        val index = bankAccountCoinsResponse.indexOfFirst { it.coinId == coin.id }
+                        if(index >= 0){
+                            val bankAccountCoin = bankAccountCoinsResponse[index]
+                            bankAccountCoinsResponse[index] = BankAccountCoin.newBuilder()
+                                .setId(bankAccountCoin.id)
+                                .setCoin(coin)
+                                .setCoinId(coin.id)
+                                .setAmount(bankAccountCoin.amount)
+                                .build()
+
+                        }else{
+                            bankAccountCoinsResponse.add(
+                                BankAccountCoin.newBuilder()
+                                    .setCoin(coin)
+                                    .setCoinId(coin.id)
+                                    .build()
+                            )
+                        }
+                    }
+
+                    sendBlocking(
+                        ListOfBankAccountCoin.newBuilder()
+                            .setCount(bankAccountCoinsResponse.size.toLong())
+                            .addAllData(bankAccountCoinsResponse)
+                            .build()
+                    )
+                }
+            }
+
+            val bankAccountCoinsRef = firestore.collection("bankaccounts").document(bankAccountId).collection("coins")
+            val bankAccountCoinsListener = bankAccountCoinsRef.addSnapshotListener { coinsSnapshot, error ->
+                if (error != null)
+                    throw error
+
+                if (coinsSnapshot != null && coinsSnapshot.documents.isNotEmpty()) {
+                    val bankAccountCoins = coinsSnapshot.documents.map {
+                        BankAccountCoin.newBuilder()
+                            .setId(it.id.toLong())
+                            .setCoinId(it.getLong("coinId")?:0L)
+                            .setAmount(it.getDouble("amount")?:0.0)
+                            .build()
+                    }
+
+                    bankAccountCoins.forEach { bankAccountCoin ->
+                        val index = bankAccountCoinsResponse.indexOfFirst { it.coinId == bankAccountCoin.coinId }
+                        if(index >= 0){
+                            val bankAccountCoinTemp = bankAccountCoinsResponse[index]
+                            bankAccountCoinsResponse[index] = BankAccountCoin.newBuilder()
+                                .setId(bankAccountCoin.id)
+                                .setCoin(bankAccountCoinTemp.coin)
+                                .setCoinId(bankAccountCoin.coinId)
+                                .setAmount(bankAccountCoin.amount)
+                                .build()
+                        }else{
+                            bankAccountCoinsResponse.add(
+                                BankAccountCoin.newBuilder()
+                                    .setId(bankAccountCoin.id)
+                                    .setAmount(bankAccountCoin.amount)
+                                    .setCoinId(bankAccountCoin.id)
+                                    .build()
+                            )
+                        }
+                    }
+
+                    sendBlocking(
+                        ListOfBankAccountCoin.newBuilder()
+                            .setCount(bankAccountCoinsResponse.size.toLong())
+                            .addAllData(bankAccountCoinsResponse)
+                            .build()
+                    )
+                }
+            }
+
+            awaitClose {
+                bankAccountCoinsListener.remove()
+                coinsListener.remove()
             }
         }
 
